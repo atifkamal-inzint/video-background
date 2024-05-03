ad99be3e68d09bf0aa67eb98de4efee4b69cf582
atifkamal-inzint <atif@inzint.com>
2024-05-03 12:23:58 +0530

    initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,33 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <div className="relative">
+          <div className="fixed inset-0 z-0 " id="background-video-div">
+            <video
+              className="h-full w-full object-cover"
+              autoPlay
+              loop
+              muted
+              controls
+              playsInline
+              id="bg-video"
+            >
+              {/* Add fallback sources here (e.g., MP4) for cross-browser compatibility */}
+              <source
+                src="https://dextoro-images.s3.ap-southeast-2.amazonaws.com/wave.mp4"
+                type="video/mp4"
+              />
+              <source
+                src="https://dextoro-images.s3.ap-southeast-2.amazonaws.com/wave.webm"
+                type="video/webm"
+              />
+            </video>
+          </div>
+          <div className="relative z-10 overflow-hidden">
+            <Main />
+            <NextScript />
+          </div>
+        </div>
       </body>
     </Html>
   );
